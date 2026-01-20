85b3ee2843a4fc5033e8b296f1fe20574e232e2c
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2026-01-20 23:59:55 +0600

    meal count api update

--- meals/urls.py
+++ meals/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MealCreateView, MealListView
+from .views import MealCreateView, MealListView,MonthlyMealCountView
 
 urlpatterns = [
     path('create/', MealCreateView.as_view()),
     path('my-meals/', MealListView.as_view()),
+    path('count/', MonthlyMealCountView.as_view()),
  
 ]
 
