288f7a2894360bd121c0c64226b1933f7b5a83ad
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2026-01-21 18:54:56 +0600

    all api run successfully

--- meals/models.py
+++ meals/models.py
@@ -37,27 +37,3 @@ class Meal(models.Model):
     def __str__(self):
         return f"{self.member.full_name} - {self.date} ({self.total_meal})"
 
-
-
-# from django.db import models
-# from members.models import MemberProfile
-
-# class Meal(models.Model):
-#     member = models.ForeignKey(MemberProfile, on_delete=models.CASCADE)
-#     date = models.DateField()
-
-#     meal_count = models.PositiveSmallIntegerField(default=1)  
-#     # 0 = off, 1-4 = meal count
-
-#     is_off = models.BooleanField(default=False)
-
-#     class Meta:
-#         unique_together = ('member', 'date')
-
-#     def save(self, *args, **kwargs):
-#         if self.is_off:
-#             self.meal_count = 0
-#         super().save(*args, **kwargs)
-
-#     def __str__(self):
-#         return f"{self.member.full_name} - {self.date} ({self.meal_count})"
